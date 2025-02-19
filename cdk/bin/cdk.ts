4704b8f1e726a037873753fa4c2b1f58a1b319b1
Sebastian Nikkonen <5937817+Sebastiannnnnn@users.noreply.github.com>
2025-02-19 17:25:58 +0200

    Add AWS EKS config to repository

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,14 +1,22 @@
 import * as cdk from "aws-cdk-lib";
+import * as dotenv from 'dotenv';
 import { createCeeveeStack } from "../lib/cdk-stack"; // Import the function
+
+dotenv.config();
 
 // Initialize CDK App
 const app = new cdk.App();
 
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+}
+
 // Create a new stack for Cognito
-const stack = new cdk.Stack(app, "CeeveeStack", { env: { region: "eu-north-1" } });
+const stack = new cdk.Stack(app, "CeeveeStack", { env });
 
 // Apply Cognito stack function
-createCeeveeStack(stack);
+createCeeveeStack(stack); 
 
 // Synthesize the stack
 app.synth();
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { KubectlV32Layer } from '@aws-cdk/lambda-layer-kubectl-v32';
 
 const createCognito = (stack: cdk.Stack) => {
   // Create Cognito User Pool
@@ -42,10 +45,53 @@ const createCognito = (stack: cdk.Stack) => {
   new cdk.CfnOutput(stack, "AuthCeeveeClientId", { value: userPoolClient.userPoolClientId });
 }
 
+const createVPC = (stack: cdk.Stack): ec2.IVpc => {
+  return new ec2.Vpc(stack, 'EksVPC', {
+    maxAzs: 2,
+    subnetConfiguration: [
+      {
+        cidrMask: 24,
+        name: 'Public',
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      {
+        cidrMask: 24,
+        name: 'Private',
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      }
+    ],
+    natGateways: 1
+  });
+};
+
+const createEKSCluster = (stack: cdk.Stack) => {
+  // Create new VPC
+  const vpc = createVPC(stack);
+
+  // Create basic EKS cluster
+  const cluster = new eks.Cluster(stack, 'EksCluster', {
+    vpc,
+    version: eks.KubernetesVersion.V1_32,
+    clusterName: 'teamit-cluster',
+    defaultCapacity: 1,
+    defaultCapacityInstance: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
+    kubectlLayer: new KubectlV32Layer(stack, 'KubectlLayer')
+  });
+
+  // Output important cluster information
+  new cdk.CfnOutput(stack, 'EksClusterEndpoint', {
+    value: cluster.clusterEndpoint,
+  });
+  new cdk.CfnOutput(stack, 'EksClusterName', {
+    value: cluster.clusterName,
+  });
+}
+
 /**
  * Function to create the AWS Cognito Stack.
  * @param stack - The CDK stack where resources will be added.
  */
 export const createCeeveeStack = (stack: cdk.Stack) => {
-  createCognito(stack)
+  createCognito(stack);
+  createEKSCluster(stack);
 }
