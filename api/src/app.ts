8fd32283ef4625e66a808a5d883d787bd9062b6c
Sebastian Nikkonen <5937817+Sebastiannnnnn@users.noreply.github.com>
2025-02-25 13:05:16 +0200

    spacing

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,4 @@
-import type {NextFunction, Request, Response} from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import express from 'express';
 import routes from './routes/index.js';
 import prisma from './db.js';
