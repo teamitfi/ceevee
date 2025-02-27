d0996a292dfdcdc75e6839800937d36dc6eeb3ec
Sebastian Nikkonen <5937817+Sebastiannnnnn@users.noreply.github.com>
2025-02-27 10:32:53 +0200

    Update CloudFront config

--- cdk/lib/stacks/cloudfront-stack.ts
+++ cdk/lib/stacks/cloudfront-stack.ts
@@ -15,24 +15,47 @@ export class CeeveeCloudFrontStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CeeveeCloudFrontStackProps) {
     super(scope, id, props);
 
-    const certificate = new acm.Certificate(this, 'Certificate', {
-      domainName: 'aiexp.fi',
-      validation: acm.CertificateValidation.fromDns(),
-    });
-
     const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
-        origin: new origins.LoadBalancerV2Origin(props.uiStack.service.loadBalancer),
+        origin: new origins.LoadBalancerV2Origin(props.uiStack.service.loadBalancer, {
+          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,  // Move this here
+        }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
       additionalBehaviors: {
         '/api/*': {
-          origin: new origins.LoadBalancerV2Origin(props.apiStack.service.loadBalancer),
+          origin: new origins.LoadBalancerV2Origin(props.apiStack.service.loadBalancer, {
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,  // Move this here
+          }),
           viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         },
       },
-      certificate: certificate,
-      domainNames: ['aiexp.fi'],
     });
+
+    // Add output for CloudFront URL
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'CloudFront Distribution Domain Name'
+    });
+
+    // const certificate = new acm.Certificate(this, 'Certificate', {
+    //   domainName: 'aiexp.fi',
+    //   validation: acm.CertificateValidation.fromDns(),
+    // });
+
+    // const distribution = new cloudfront.Distribution(this, 'Distribution', {
+    //   defaultBehavior: {
+    //     origin: new origins.LoadBalancerV2Origin(props.uiStack.service.loadBalancer),
+    //     viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    //   },
+    //   additionalBehaviors: {
+    //     '/api/*': {
+    //       origin: new origins.LoadBalancerV2Origin(props.apiStack.service.loadBalancer),
+    //       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    //     },
+    //   },
+    //   certificate: certificate,
+    //   domainNames: ['aiexp.fi'],
+    // });
   }
 }
